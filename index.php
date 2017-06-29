f22c6c0c67d2962981c272c9122f1c7fc3af3fed
MarlonGuarnieri <marlonguarnieri@gmail.com>
2017-06-29 10:27:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<div class="social">
+
+		<a href="" class="sair">sair</a>
+
+		<img src="http://www.laserskincare.ae/wp-content/uploads/2015/01/Untitled-3.jpg" alt="" width="200" height="200">
+		<h3>Bem vindo!</h3>
+	</div>
+
+</body>
+</html>
---
+++ verifica_usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+	var_dump($_POST);
+	exit();
+
+	session_start();
+
+	if () {
+	
+	} else {
+
+	}
